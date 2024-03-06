5ff382471021b035170cadf1a4cbae01d73e0460
jujubooom <1060089371@qq.com>
2024-03-06 12:51:34 +0800

    Site updated: 2024-03-06 12:51:33

---
+++ links/val.js
@@ -0,0 +1,9 @@
+<script src='//unpkg.com/valine/dist/Valine.min.js'></script>
+<div id="vcomments"></div>
+<script>
+new Valine({
+			el: '#vcomments',
+			appId: '8LjCrfCBHCeaJE4iBTiGLna6-MdYXbMMI',
+			appKey: 'ZCjgBZprrWRhU2s6Fgblj9KY',
+		})
+</script>
