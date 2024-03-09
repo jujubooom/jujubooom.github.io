705082b18bdacc829425ce06696b12d8a1a0925c
jujubooom <1060089371@qq.com>
2024-03-09 13:38:47 +0800

    Site updated: 2024-03-09 13:38:47

--- js/page/category-page.js
+++ js/page/category-page.js
@@ -1,6 +1,68 @@
-function resetCategoriesPage(){const r=e=>{e.forEach(e=>{var t=e.querySelector(".site-all-category-list-link"),l=e.querySelector(".site-all-category-list-count"),s=e.querySelector(".site-all-category-list-child");let i=`
+/* global KEEP */
+
+function resetCategoriesPage() {
+  const resetCategoryDom = (domList) => {
+    domList.forEach((dom) => {
+      const categoryNameDom = dom.querySelector('.site-all-category-list-link')
+      const categoryCountDom = dom.querySelector('.site-all-category-list-count')
+      const childCategoryInfo = dom.querySelector('.site-all-category-list-child')
+
+      let domTemplate = `
             <div class="self-category-info border-box">
-              <div class="left border-box">${s?'<i class="icon border-box fa-regular fa-square-plus"></i> ':""}${t.outerHTML}</div>
-              <div class="right border-box">${l.outerHTML}</div>
+              <div class="left border-box">${
+                childCategoryInfo
+                  ? '<i class="icon border-box fa-regular fa-square-plus"></i> '
+                  : ''
+              }${categoryNameDom.outerHTML}</div>
+              <div class="right border-box">${categoryCountDom.outerHTML}</div>
             </div>
-          `;s&&(r(s.querySelectorAll(".site-all-category-list-item")),i+=s.outerHTML),e.innerHTML=i})};r(document.querySelectorAll(".site-all-category-list .site-all-category-list-item")),document.querySelectorAll(".site-all-category-list-item").forEach(e=>{let t=!1;const l=e.querySelector(".self-category-info .left .icon"),s=e.querySelector(".site-all-category-list-child");l&&l.addEventListener("click",()=>{t=!t,s&&(t?(s.style.height="auto",s.style.visibility="visible",l.classList.add("fa-square-minus"),l.classList.remove("fa-square-plus")):(s.style.height="0",s.style.visibility="hidden",l.classList.add("fa-square-plus"),l.classList.remove("fa-square-minus")))})})}!0===KEEP.theme_config.pjax.enable&&KEEP.utils?resetCategoriesPage():window.addEventListener("DOMContentLoaded",resetCategoriesPage);
+          `
+
+      if (childCategoryInfo) {
+        resetCategoryDom(childCategoryInfo.querySelectorAll('.site-all-category-list-item'))
+        domTemplate += childCategoryInfo.outerHTML
+      }
+
+      dom.innerHTML = domTemplate
+    })
+  }
+
+  const expandHandle = () => {
+    const selfCategoryInfoDom = document.querySelectorAll('.site-all-category-list-item')
+    selfCategoryInfoDom.forEach((dom) => {
+      let isExpand = false
+      const iconDom = dom.querySelector('.self-category-info .left .icon')
+      const childDom = dom.querySelector('.site-all-category-list-child')
+
+      iconDom &&
+        iconDom.addEventListener('click', () => {
+          isExpand = !isExpand
+          if (childDom) {
+            if (isExpand) {
+              childDom.style.height = 'auto'
+              childDom.style.visibility = 'visible'
+              iconDom.classList.add('fa-square-minus')
+              iconDom.classList.remove('fa-square-plus')
+            } else {
+              childDom.style.height = '0'
+              childDom.style.visibility = 'hidden'
+              iconDom.classList.add('fa-square-plus')
+              iconDom.classList.remove('fa-square-minus')
+            }
+          }
+        })
+    })
+  }
+
+  resetCategoryDom(
+    document.querySelectorAll('.site-all-category-list .site-all-category-list-item')
+  )
+
+  expandHandle()
+}
+
+if (KEEP.theme_config.pjax.enable === true && KEEP.utils) {
+  resetCategoriesPage()
+} else {
+  window.addEventListener('DOMContentLoaded', resetCategoriesPage)
+}
